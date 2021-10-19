a949bfead18b75a4b4e64bfe4e4ee037118ff15c
Mentor24 <mk.burdach@gmail.com>
2021-10-19 21:16:28 +0200

    changes_v1

---
+++ script.js
@@ -0,0 +1,34 @@
+const task1a = () => {
+    for (i=1; i<=9; i++)
+    console.log(i)
+    }
+
+const task1b = () => {
+    for (i=9; i>=1; i--)
+    console.log(i)
+    }
+
+const task1c = () => {
+    for (i=5; i<=15; i++)
+    console.log(i)
+    }
+
+const task1d = () => {
+    for (i=0; i>=-10; i--)
+    console.log(i)
+    }
+
+const task1e = () => {
+    for (i=1; i<20; i+=2)
+    console.log(i)
+    }
+
+const task1f = () => {
+    for (i=10; i>=-10; i-=4)
+    console.log(i)
+    }
+
+const task1g = () => {
+    for (i=0; i>=100; i*=2)
+    console.log(i)
+    }
